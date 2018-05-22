bf9c7c767459594fc3044d45860c02fb1ab97bda
Evangel Teo <evangelteo@gmail.com>
2018-05-22 19:18:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,19 +14,17 @@ const codes = [
 function init() {
   function konami(e) {
     var codescopy = codes
-    if (e.key == codescopy[0]) {
+    while(e.key == codescopy[0]) {
       codescopy.shift()
       if (codescopy.length === 0) {
       alert('Congrats!')
     }
-    }
     else {
       codescopy = codes.splice()
     }
-    if (codescopy.length === 0) {
-      alert('Congrats!')
     }
   }
+}
 
     
     document.body.addEventListener('keydown', konami)
